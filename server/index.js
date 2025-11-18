00d52a0fd1517b574f531318cadac501d802e503
IRFAN-KHAN-git <irfanprof9@gmail.com>
2025-11-18 09:23:41 +0530

    another update

--- server/index.js
+++ server/index.js
@@ -23,8 +23,8 @@ mongoose
   .connect(process.env.DBURL)
   .then(()=>{
     console.log("Connected to DataBase");
-    app.listen(process.env.BASE_URL ,() => {
-      console.log(`server is running:= ${process.env.BASE_URL}`);
+     app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server running on port ${process.env.PORT || 8000}`);
     });
     
   })
